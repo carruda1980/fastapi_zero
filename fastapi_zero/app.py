9e7f7f4132c6ab184139f631fc85106d6c195fe1
Carlos Arruda <caugustogarruda@gmail.com>
2025-07-09 13:53:33 -0300

    Iniciando projeto

---
+++ fastapi_zero/app.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
+
+app = FastAPI()
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá html!</title>
+      </head>
+      <body>
+        <h1> Olá html </h1>
+      </body>
+    </html>"""
---
+++ tests/test_app.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_deve_retornar_ola_html():
+    client = TestClient(app)
+
+    response = client.get('/exercicio-html')
+
+    assert '<h1> Olá html </h1>' in response.text
