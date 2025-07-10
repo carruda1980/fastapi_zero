7467cb690c7e35c78f13ece45f4b1428f8b48da2
Carlos Arruda <caugustogarruda@gmail.com>
2025-07-10 13:12:33 -0300

    Implementa retorno esperado para o create

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -27,4 +27,5 @@ def exercicio_html():
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema): ...
+def create_user(user: UserSchema):
+    return user
